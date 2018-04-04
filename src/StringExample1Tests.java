81fede90f51d8087876af3b1301829946fd9e481
Josh Vargas <jvargas12@gmail.com>
2018-04-04 00:36:44 -0400

    Added test

---
+++ src/StringExample1Tests.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringExample1Tests {
+
+	@Test
+    public void noWordsShowsZero() {
+
+		String as ="";
+        // assert statements
+        assertEquals("", StringExample1.returnLongestWord(as), "No words is 0");
+        
+    }
+    
+}
