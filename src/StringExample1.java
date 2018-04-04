2f4e727e4d87c05194ec2f05f3bd94e382657c92
Josh Vargas <jvargas12@gmail.com>
2018-04-04 10:22:37 -0400

    Formatting

--- src/StringExample1.java
+++ src/StringExample1.java
@@ -2,41 +2,37 @@
 public class StringExample1 {
 
 	public static void main(String[] args) {
-		
-		 String exampleSentence = "The cow jumped over the moon";
-		 returnLongestWord(exampleSentence);
-	       
-	    }
-	 
-	 public static String returnLongestWord (String sentence) {
-	        
-		 
-		 //  Assumptions
-		 //  1. Special characters are included in the length of a word.
-		 //  2. Numbers are included in the length of the word.
-		 //  3. Spaces separate words from each other
 
+		String exampleSentence = "The cow jumped over the moon";
+		returnLongestWord(exampleSentence);
 
-	        String longestWord ="";
-	        String splitWords [] = sentence.split(" ");
-	          
-	        for (int i=0; i<splitWords.length; i++){
-	           
-	        		String currentWord = splitWords[i].toLowerCase();
-	            int currentLength = currentWord.length();
-	            int longestLength = longestWord.length();
-	         
-	            
-	            if(currentLength > longestLength){
-	                longestWord = currentWord;
-	            }
-	            else{}
-	        
-	        
-	         }
-	        System.out.println(longestWord.length() + "  " + longestWord);
-	        return longestWord;
-		 
-	 }
+	}
+
+	public static String returnLongestWord(String sentence) {
+
+		// Assumptions
+		// 1. Special characters are included in the length of a word.
+		// 2. Numbers are included in the length of the word.
+		// 3. Spaces separate words from each other
+
+		String longestWord = "";
+		String splitWords[] = sentence.split(" ");
+
+		for (int i = 0; i < splitWords.length; i++) {
+
+			String currentWord = splitWords[i].toLowerCase();
+			int currentLength = currentWord.length();
+			int longestLength = longestWord.length();
+
+			if (currentLength > longestLength) {
+				longestWord = currentWord;
+			} else {
+			}
+
+		}
+		System.out.println(longestWord.length() + "  " + longestWord);
+		return longestWord;
+
+	}
 
 }
--- src/StringExample1Tests.java
+++ src/StringExample1Tests.java
@@ -6,40 +6,40 @@ class StringExample1Tests {
 
 	@Test
 	public void noWordsShowsZero() {
-		String testString ="";
+		String testString = "";
 		assertEquals("", StringExample1.returnLongestWord(testString), "No words is 0");
-        
-    }
-	
+
+	}
+
 	@Test
-    public void longestWord() {
-    		String testString ="this is the longest word";
-        assertEquals("longest", StringExample1.returnLongestWord(testString), "Longest is the longest word");
-        
-    }
-    
-    @Test
-    public void firstWordIsTheLongestTest() {
-    		String testString = "Latitude is the longest";
-    		assertEquals("latitude", StringExample1.returnLongestWord(testString), "First word can be longest");
-    }
-    
-    @Test
-    public void lastWordIsTheLongestTest() {
-    		String testString = "this is the longest";
-    		assertEquals("longest", StringExample1.returnLongestWord(testString), "Last word can be longest");
-    }
-    
-    @Test
-    public void ignoreCapitalizationTest() {
-    		String testString = "this is AaaAAaaAaAaA sentence";
-    		assertEquals("aaaaaaaaaaaa", StringExample1.returnLongestWord(testString), "Ignores capitalization");
-    }
-    
-    @Test
-    public void allowSpecialCharactersTest() {
-    		String testString = "special !@#$%^&*():";
-    		assertEquals("!@#$%^&*():", StringExample1.returnLongestWord(testString), "Allow special characters");
-    }
-    
+	public void longestWord() {
+		String testString = "this is the longest word";
+		assertEquals("longest", StringExample1.returnLongestWord(testString), "Longest is the longest word");
+
+	}
+
+	@Test
+	public void firstWordIsTheLongestTest() {
+		String testString = "Latitude is the longest";
+		assertEquals("latitude", StringExample1.returnLongestWord(testString), "First word can be longest");
+	}
+
+	@Test
+	public void lastWordIsTheLongestTest() {
+		String testString = "this is the longest";
+		assertEquals("longest", StringExample1.returnLongestWord(testString), "Last word can be longest");
+	}
+
+	@Test
+	public void ignoreCapitalizationTest() {
+		String testString = "this is AaaAAaaAaAaA sentence";
+		assertEquals("aaaaaaaaaaaa", StringExample1.returnLongestWord(testString), "Ignores capitalization");
+	}
+
+	@Test
+	public void allowSpecialCharactersTest() {
+		String testString = "special !@#$%^&*():";
+		assertEquals("!@#$%^&*():", StringExample1.returnLongestWord(testString), "Allow special characters");
+	}
+
 }
