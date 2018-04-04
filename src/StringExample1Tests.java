0c840d34acaebeee2e8f8e0db0332eadd64bb8d8
Josh Vargas <jvargas12@gmail.com>
2018-04-04 00:44:27 -0400

    Adding more tests for string example

--- src/StringExample1Tests.java
+++ src/StringExample1Tests.java
@@ -6,9 +6,34 @@ class StringExample1Tests {
 
 	@Test
 	public void noWordsShowsZero() {
-		String as ="";
-        assertEquals("", StringExample1.returnLongestWord(as), "No words is 0");
+		String testString ="";
+		assertEquals("", StringExample1.returnLongestWord(testString), "No words is 0");
         
+    }
+	
+	@Test
+    public void longestWord() {
+    		String testString ="this is the longest word";
+        assertEquals("longest", StringExample1.returnLongestWord(testString), "Longest is the longest word");
+        
+    }
+    
+    @Test
+    public void firstWordIsTheLongestTest() {
+    		String testString = "Latitude is the longest";
+    		assertEquals("latitude", StringExample1.returnLongestWord(testString), "First word is the longest string");
+    }
+    
+    @Test
+    public void ignoreCapitalizationTest() {
+    		String testString = "this is AaaAAaaAaAaA sentence";
+    		assertEquals("aaaaaaaaaaaa", StringExample1.returnLongestWord(testString), "Ignores capitalization");
+    }
+    
+    @Test
+    public void allowSpecialCharactersTest() {
+    		String testString = "special !@#$%^&*():";
+    		assertEquals("!@#$%^&*():", StringExample1.returnLongestWord(testString), "Allow special characters");
     }
     
 }
